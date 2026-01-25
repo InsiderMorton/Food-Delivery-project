613a8a25a6e550557beb6b27709bc56350d3f0d7
Insider Morton <insidermarlow@gmail.com>
2026-01-25 22:18:39 +0600

    Milestone 1

--- src/edu/aitu/oop3/db/DatabaseConnection.java
+++ src/edu/aitu/oop3/db/DatabaseConnection.java
@@ -4,19 +4,16 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-// Renamed from Database_connection to DatabaseConnection to match Main.java
 public class DatabaseConnection {
 
-    // Your Supabase credentials
-    private static final String URL = "jdbc:postgresql://aws-1-ap-northeast-2.pooler.supabase.com:5432/postgres";
-    private static final String USER = "postgres.yncbsdetxhyvpvgdahcm";
-    private static final String PASSWORD = "Maga2708!!!!";
-
     private DatabaseConnection() {
-        // Private constructor to prevent instantiation
     }
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        return DriverManager.getConnection(
+                DatabaseConfig.getUrl(),
+                DatabaseConfig.getUser(),
+                DatabaseConfig.getPassword()
+        );
     }
 }
